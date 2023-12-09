b19ad5ee80c6d8019304ee19a6743758197965b5
conbanwa <j6j7@qq.com>
2023-12-09 23:18:37 +0800

    Update log_test.go

--- test/log_test.go
+++ test/log_test.go
@@ -8,13 +8,15 @@ import (
 )
 
 func TestLog(t *testing.T) {
-	os.Setenv("LOG_LEVEL", "DEBUG")
+	os.Setenv("LOG_LEVEL", "WARN")
 	logs.I("hello world")
 	logs.Blue("hello world")
+	logs.Log.Level = logs.L_ERROR
 	for i := 0; i < 10; i++ {
 		logs.Inline("hello world")
 	}
 	logs.Green("hello world")
 	logs.I("hello world")
 	logs.Blue("hello world")
+	logs.W("hello world")
 }
